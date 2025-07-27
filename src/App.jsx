0dde1f17823918d3fe7932f0f813bcc69cb72d18
sachinyaganti <2300032331@kluniversity.in>
2025-07-27 23:42:33 +0530

    V1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S219_CICD">
+    <Router basename="/cicd-ecom">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
